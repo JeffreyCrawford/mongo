391f425c208db30a5cc7e948608023064d95a507
Jeff Crawford <jeffrey.a.crawford@gmail.com>
2018-06-07 16:24:33 -0400

    again

--- server.js
+++ server.js
@@ -20,12 +20,12 @@ app.use(express.static("./app/public"));
 
 var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/newspaper";
 
-mongoose.Promise = Promise;
-mongoose.connect(MONGODB_URI);
+
 
 
 
 require("./app/routes/api-routes")(app);
 require("./app/routes/html-routes")(app);
 
-//
+mongoose.Promise = Promise;
+mongoose.connect(MONGODB_URI);
