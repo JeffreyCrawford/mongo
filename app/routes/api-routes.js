931aaa2496b760722ef1d5c01d922354c823fdd7
Jeff Crawford <jeffrey.a.crawford@gmail.com>
2018-06-07 05:36:00 -0400

    external api routes

--- app/routes/api-routes.js
+++ app/routes/api-routes.js
@@ -1,8 +1,61 @@
-var db = require("../models")
 
-console.log(db)
+var mongojs = require("mongojs")
+var connectionString = "newspaper";
+var collections = ["articles"];
+var request = require("request");
+var cheerio = require("cheerio");
+var Article = require("../models/article")
+var Comment = require("../models/comment")
+var db = mongojs(connectionString, collections);
+
 
 
 module.exports = function(app) {
-    
+
+
+    app.get("/scrape", function(req, res) {
+
+        request("https://www.economist.com/latest-updates", function(error, response, html) {
+            var $ = cheerio.load(html);
+
+            var results = [];
+
+            $(".teaser__group-text").each(function(i, element) {
+                
+                var flytitle = $(element).children(".flytitle-and-title__body").children(".flytitle-and-title__flytitle").text()
+                var title = $(element).children(".flytitle-and-title__body").children(".flytitle-and-title__title").text();
+                var summary = $(element).children(".teaser__text").text();
+                var url = $(element).parent().attr("href");
+
+                results.push({
+                    flytitle: flytitle,
+                    title: title,
+                    summary: summary,
+                    url: `https://www.economist.com${url}`
+                })
+            })
+
+            Article.create(results)
+            .then(function(dbArticle) {
+                db.articles.insert(dbArticle)
+                console.log(dbArticle)
+            })
+            .catch(function(err) {
+                return res.json(err);
+            })
+            
+        });
+    });
+
+    app.get("/all", function(req, res) {
+        db.articles.find({}, function(error, found) {
+            if (error) {
+                console.log(error)
+            }
+            else {
+                res.json(found)
+            }
+        })
+    })
+
 }
--- server.js
+++ server.js
@@ -7,72 +7,25 @@ var mongojs = require("mongojs")
 var Article = require("./app/models/article")
 
 
+
 var app = express();
+
+
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("./app/public"));
 
 
-var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+
+
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/newspaper";
 
 mongoose.Promise = Promise;
 mongoose.connect(MONGODB_URI);
 
-var connectionString = "newspaper";
-var collections = ["articles"];
-
-var db = mongojs(connectionString, collections);
 
 
-
-
-app.get("/scrape", function(req, res) {
-
-    request("https://www.economist.com/latest-updates", function(error, response, html) {
-        var $ = cheerio.load(html);
-
-        var results = [];
-
-        $(".teaser__group-text").each(function(i, element) {
-            
-            var flytitle = $(element).children(".flytitle-and-title__body").children(".flytitle-and-title__flytitle").text()
-            var title = $(element).children(".flytitle-and-title__body").children(".flytitle-and-title__title").text();
-            var summary = $(element).children(".teaser__text").text();
-            var url = $(element).parent().attr("href");
-
-            results.push({
-                flytitle: flytitle,
-                title: title,
-                summary: summary,
-                url: `https://www.economist.com${url}`
-            })
-        })
-
-        Article.create(results)
-        .then(function(dbArticle) {
-            db.articles.insert(dbArticle)
-            console.log(dbArticle)
-        })
-        .catch(function(err) {
-            return res.json(err);
-        })
-        
-    });
-});
-
-app.get("/all", function(req, res) {
-    db.articles.find({}, function(error, found) {
-        if (error) {
-            console.log(error)
-        }
-        else {
-            res.json(found)
-        }
-    })
-})
-
-
-
+require("./app/routes/api-routes")(app);
 
 
 //
